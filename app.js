2cf4de3e4e3878d74e4c295a9e99a04f75334819
Ara <ayaghsizian@gmail.com>
2017-07-24 23:30:00 -0400

    chuck norris api is working

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-$( "h1" ).html( "Next Step..." )
+$( "h2" ).html( "jQuery is working..." );
+$(document).ready(function(){
+
+  var chuckButton = $( "#chuckButton" );
+  chuckButton.on( "click", function(  ) {
+    $.ajax({
+       url: "http://api.icndb.com/jokes/random?firstName=Chuck&amp;lastName=Norris",
+       data: {format: 'json'},
+       error: function(event) {console.log('An error has occurred: ', event);},
+       dataType: 'jsonp',
+       success: function( result ) {
+         console.log(result);
+         $( "#chuckQuote" ).html( "<strong>" + result.value.joke + "</strong>" );
+       },
+       type: 'GET'
+    });//chuck ajax
+  });//chuck click
+
+
+  
+});// doc ready
